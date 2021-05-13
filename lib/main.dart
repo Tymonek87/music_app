3f1a1a2a7042cfc54064280dda8149788fb7828c
Tymonek87 <83309458+Tymonek87@users.noreply.github.com>
2021-05-13 09:58:22 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:music_app/playerControls.dart';
+import 'navbar.dart';
+import 'albumart.dart';
+import 'color.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double sliderValue = 2;
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Navbar(),
+          Container(
+            height: height / 2.5,
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return AlbumArt();
+              },
+              itemCount: 3,
+              scrollDirection: Axis.horizontal,
+            ),
+          ),
+          Text(
+            'Gidget',
+            style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w500,
+                color: darkPrimaryColor),
+          ),
+          Text(
+            'Gooofklas',
+            style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.w400,
+                color: darkPrimaryColor),
+          ),
+          SliderTheme(
+            data: SliderThemeData(
+              trackHeight: 5,
+              thumbShape: RoundSliderThumbShape(enabledThumbRadius: 5)
+            ),
+            child: Slider(
+              value: sliderValue,
+              activeColor: darkPrimaryColor,
+              inactiveColor: darkPrimaryColor.withOpacity(0.3),
+              onChanged: (value) {
+                setState(() {
+                  sliderValue = value;
+                });
+              },
+              min: 0,
+              max: 20,
+            ),
+          ),
+          PlayerControls(),
+          SizedBox(height: 100,)
+        ],
+      ),
+    );
+  }
+}
